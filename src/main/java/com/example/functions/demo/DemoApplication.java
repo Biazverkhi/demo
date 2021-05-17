193fabaa7ee621277600171268ad2e3ab7c9df2d
Aliaksei Biazverkhi <a.biazverkhi@gmail.com>
2021-05-17 22:05:17 +0300

    init

--- src/main/java/com/example/functions/demo/DemoApplication.java
+++ src/main/java/com/example/functions/demo/DemoApplication.java
@@ -7,12 +7,8 @@ import org.springframework.cloud.function.context.MessageRoutingCallback;
 import org.springframework.cloud.function.context.config.RoutingFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -20,8 +16,10 @@ import java.util.function.Function;
 public class DemoApplication {
     @Autowired
     RoutingFunction route;
+
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(DemoApplication.class, args); }
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
     @Bean
     public MessageRoutingCallback customRouter() {
@@ -29,10 +27,10 @@ public class DemoApplication {
             @Override
             public String functionDefinition(Message<?> message) {
                 System.out.println(message.getHeaders().toString());
-               Object obj= message.getHeaders().get("function_name");
-               if(obj instanceof List){
-                 return   ((List<?>) obj).get(0).toString();
-               }
+                Object obj = message.getHeaders().get("function_name");
+                if (obj instanceof List) {
+                    return ((List<?>) obj).get(0).toString();
+                }
                 return (String) obj;
             }
         };
@@ -41,22 +39,36 @@ public class DemoApplication {
     @Bean
     public Function<Message<String>, Object> enter() {
         System.out.println("enter1");
-       return value -> {
-           System.out.println(value.getHeaders().toString());
-           return route.apply(MessageBuilder.withPayload(value.getPayload()).setHeader("function_name", "concat").build());}
-    ;}
+        return value -> {
+            System.out.println(value.getHeaders().toString());
+            return route.apply(MessageBuilder.withPayload(value.getPayload()).setHeader("function", "concat").build());
+        };
+    }
 
 
     @Bean
-    public Function<Message<String>, String> concat() {
+    public Function<Message<Object>, String> concat() {
+
         System.out.println("concat1");
-        return value -> value.getPayload().concat("dd"); }
-//
-//    @Bean
-//    public Function<Message<String>, String> lowerCase() {
-//        System.out.println("lowerCase1");
-//        return value -> value.getPayload().toLowerCase();
-//    }
+        return value -> {
+            value.getHeaders();
+            Object multipartFiles =value.getPayload();
+            System.out.println("test!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println(multipartFiles);
+
+            return null;
+
+
+        };
+    }
+
+    @Bean
+    public Function<Message<Object>, String> lowerCase() {
+        System.out.println("lowerCase1");
+        return value -> {
+            System.out.println("test22222222222222222");
+           return value.getPayload().toString().toLowerCase();};
+    }
 
 //    @Autowired
 //    private BeanFactoryMessageChannelDestinationResolver resolver;
@@ -71,7 +83,8 @@ public class DemoApplication {
 
 //
 //curl -X POST localhost:8080 -d "Hello" -H "function_name: concat" -H "Content-Type: text/plain"
-//curl -X POST https://us-central1-my-project-228500.cloudfunctions.net/demo -d "Hello" -H "function_name: concat" -H "Content-Type: text/plain"
+//curl -X POST localhost:8080 -d "Hello" -H "spring.cloud.function.definition: concat" -H "Content-Type: text/plain"
+//curl -X POST https://us-central1-my-project-228500.cloudfunctions.net/demo -d "Hello" -H "function_name: concat" -H "spring.cloud.function.definition: concat" -H "Content-Type: text/plain"
 
 
 }
